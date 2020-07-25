64766600fbb4deefbcf078b90d8be4506057efae
ilowski <wojtek-ilowski@wp.pl>
2020-07-25 17:06:16 +0200

    created productFacade interface

---
+++ src/main/java/api/ProductFacade.java
@@ -0,0 +1,11 @@
+package api;
+
+import entity.Product;
+
+import java.util.List;
+
+public interface ProductFacade {
+    String createProduct(Product product);
+    String removeProduct (String productName);
+    List<Product> getAllProducts();
+}
