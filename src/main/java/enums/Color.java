85dca44816f1611c1efebf87ae1cad64d0289dbb
= <wojtek-ilowski@wp.pl>
2020-07-27 11:25:31 +0200

    added colors names

--- src/main/java/enums/Color.java
+++ src/main/java/enums/Color.java
@@ -1,16 +1,18 @@
 package enums;
 
 public enum Color {
-    BLACK("#000000"), WHITE("#FFFFFF"), RED("#FF0000"), GREEN("#008000"), BLUE("#0000FF"), YELLOW("#FFFF00");
+    BLACK("#000000","Black"), WHITE("#FFFFFF","White"), RED("#FF0000","Red"), GREEN("#008000","Green"), BLUE("#0000FF","Blue"), YELLOW("#FFFF00","Yellow");
 
     private String hex;
+    private String name;
 
-    Color(String hex) {
+    Color(String hex, String name) {
         this.hex = hex;
+        this.name = name;
     }
 
     public String getColor() {
-        return getClass().getName();
+        return name;
     }
 
     public String getHex() {
